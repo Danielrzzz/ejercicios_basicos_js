303a4e482084b530d357a7d14bd56fa225134b1d
Danielrzzz <daniel.rodriguez2999@gmail.com>
2025-07-30 16:58:15 +0200

    cambios_ejercicio4

--- ejercicio2.js
+++ ejercicio2.js
@@ -1,18 +1,18 @@
-//1.1
+//2.1
 const jedi = {nombre: "Luke Skywalker", edad: 19};
 
 jedi.edad = 25;
 
 console.log(jedi);
 
-//1.2
+//2.2
 let nombre = "Leia";
 let apellido = "Organa";
 let edad = 20;
 
 console.log(`Soy ${nombre} ${apellido}, tengo ${edad} años y soy una princesa de Alderaan.`);
 
-//1.3
+//2.3
 const sable1 = {nombre: "Shoto de Yoda", precio: 1500};
 const sable2 = {nombre: "Sable de Darth Vader", precio: 2000};
 
@@ -20,7 +20,7 @@ let precioTotal = sable1.precio + sable2.precio;
 
 console.log(`El precio total de los sables de luz es ${precioTotal}.`);
 
-//1.4
+//2.4
 let precioBaseGlobal = 10000;
 
 precioBaseGlobal = 25000;
