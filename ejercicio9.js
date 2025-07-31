056b0205ee20d272939067c505de26e7ec1761f0
Danielrzzz <daniel.rodriguez2999@gmail.com>
2025-07-31 14:11:53 +0200

    ejercicio9.

--- ejercicio9.js
+++ ejercicio9.js
@@ -1,5 +1,14 @@
 const numbers = [1, 2, 3, 5, 45, 37, 58];
 
 function sumNumbers(numberList) {
-  // Completar código
+  let numTotal = 0;
+  for(let i = 0;i < numberList.length; i++) {
+    let number = numberList[i];
+    if (number > 0) {
+      numTotal = numTotal + number;
+    }
+  }
+  return numTotal;
 }
+
+console.log(sumNumbers(numbers));
