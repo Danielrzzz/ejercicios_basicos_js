9e4fe4fd904f052123f9279c6986f24497d72bed
Danielrzzz <daniel.rodriguez2999@gmail.com>
2025-07-31 14:58:26 +0200

    falta_solucionar_errores_ejercicio11

---
+++ ejercicio11.js
@@ -0,0 +1,16 @@
+const mixedElements = [6, 1, 'Marvel', 1, 'hamburguesa', '10', 'Prometeo', 8, 'Hola mundo'];
+function averageWord(list) {
+    let sumTotal = 0;
+    let promedio = 0
+    for (let i = 0;i < list.length; i++) {
+        if(typeof list[i] === "string") {
+            sumTotal = list[i].length + sumTotal;
+        } else {
+            sumTotal = list[i].length + sumTotal;
+        }
+    }
+    promedio = sumTotal / list.length;
+    return promedio;
+}
+
+console.log(averageWord(mixedElements));
