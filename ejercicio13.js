b49374103ecfd52157b724ceefba64791c126ad5
Danielrzzz <daniel.rodriguez2999@gmail.com>
2025-08-01 16:19:28 +0200

    ejercicio13_corregido

---
+++ ejercicio13.js
@@ -0,0 +1,23 @@
+const names = [
+  'Peter',
+  'Steve',
+  'Tony',
+  'Natasha',
+  'Clint',
+  'Logan',
+  'Xabier',
+  'Bruce',
+  'Peggy',
+  'Jessica',
+  'Marc'
+];
+function nameFinder(nameList, name) {
+    let validate = false;
+    for(let i = 0;i < nameList.length; i++){
+        if (name === nameList[i]) {
+            return true;
+        }
+    }
+    return false;
+}
+console.log(nameFinder(names, 'Bruce'));
