2b5710c846433d03aa38e4d78b5b757f07c91c06
Danielrzzz <daniel.rodriguez2999@gmail.com>
2025-08-01 17:45:47 +0200

    ejercicio14_comenzado_faltan_correcciones

---
+++ ejercicio14.js
@@ -0,0 +1,23 @@
+const words = [
+  'code',
+  'repeat',
+  'eat',
+  'sleep',
+  'code',
+  'enjoy',
+  'sleep',
+  'code',
+  'enjoy',
+  'sleep',
+  'code'
+];
+function repeatCounter(list) {
+    let word = "";
+    for(let i = 0;i < list.length; i++) {
+        word = list[i];
+        if () {
+
+        }
+    }
+}
+console.log(repeatCounter(words));
